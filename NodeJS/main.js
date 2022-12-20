ba134439fd6f926ac4f5460fb8fbb4e2b9213b39
manuellpz <manuelmzt44@gmail.com>
2022-12-19 18:22:05 -0700

    My First Module

--- NodeJS/main.js
+++ NodeJS/main.js
@@ -1,4 +1,9 @@
 //La funcion require() nos permite importar un modulo colocando su ruta
-const usuario = require('./modules/myModule');
+// const usuario = require('./modules/myModule');
 
-console.log(usuario.email);
+const math = require("./Math/index");
+
+console.log(math.add(5, 10));
+console.log(math.substract(5, 10));
+console.log(math.multiply(5, 10));
+console.log(math.divide(5, 10));
