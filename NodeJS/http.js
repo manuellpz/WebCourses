b716c84dba444011c6a55032fc5a28a2cf12fab4
manuellpz <manuelmzt44@gmail.com>
2023-01-06 17:55:12 -0700

    Navigation (request)

--- NodeJS/http.js
+++ NodeJS/http.js
@@ -2,7 +2,19 @@ const http = require('http');
 
 //el modulo http entre otras cosas, nos permite crear servidores
 http.createServer((request,response)=>{
-    response.write('I am a web server');
+    if(request.url === '/') {
+        response.write('Welcome to the server');
+        return response.end();
+    }
+    if(request.url === '/about') {
+        response.write('Acerca De');
+        return response.end();
+    }
+    response.write(`
+        <h1>NOT FOUND</h1>
+        <p>The page was not found</p>
+        <a href="/">Main Page</a>
+    `);
     response.end();
 }).listen(3000);
 
