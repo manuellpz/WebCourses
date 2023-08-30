babf0e732334881f56e5b6a84f9b78a2b6d8e408
Manuel Lopez <manuelmzt44@gmail.com>
2023-08-29 19:27:00 -0600

    Creating GET

--- NodeJS-Midu-API/app.js
+++ NodeJS-Midu-API/app.js
@@ -2,14 +2,33 @@ const express = require('express')
 
 const app = express()
 
+const movies = require('./movies.json')
+
 app.disable('x-powered-by')
 
-app.get('/',(req,res) => {
-   res.json({message:'Hello There'})
+//Recuperamos todas las peliculas
+app.get('/movies', (req, res) => {
+   const {genre} = req.query
+   if(genre) {
+      const filteredMovies = movies.filter(
+         movie => movie.genre.includes(genre)
+      )
+      return res.json(filteredMovies)
+   }
+   
+   res.json(movies)
+})
+
+//Recuperamos las peliculas por un ID
+app.get('/movies/:id', (req, res) => {
+   const { id } = req.params
+   const movie = movies.find(movie => movie.id === id)
+   if(movie) return res.json(movie)
+   res.status(404).json({message:'Movie Not Found'})
 })
 
 const PORT = process.env.PORT ?? 1234
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
    console.log(`Server listening on http://localhost:${PORT}`)
 })
