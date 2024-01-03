0f265f148f1cb96f2c493b144c042e5e6b15c9d3
Manuel Lopez <manuellpz@gmail.com>
2024-01-03 16:45:00 -0700

    Programacion Del Formulario

--- React-Ejercicios/src/components/SongForm.jsx
+++ React-Ejercicios/src/components/SongForm.jsx
@@ -1,7 +1,49 @@
-const SongForm = ({handleSearch}) => {
+import React, { useState, useEffect } from "react";
+
+const initialForm = {
+  artist: "",
+  song: "",
+};
+
+const SongForm = ({ handleSearch }) => {
+  const [form, setForm] = useState(initialForm);
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]:e.target.value
+    })
+  };
+  const handleSubmit = e => {
+    e.preventDefault()
+    if(!form.artist || !form.song) {
+      alert("Datos Incompletos, Favor de verificar los campos")
+      return;
+    }
+
+    handleSearch(form)
+    setForm(initialForm)
+  }
+
   return (
     <>
-      <h2>Form</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="artist"
+          placeholder="Nombre del artista: "
+          onChange={handleChange}
+          value={form.artist}
+        />
+        <input
+          type="text"
+          name="song"
+          placeholder="Nombre de la cancion: "
+          onChange={handleChange}
+          value={form.song}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
     </>
   );
 };
