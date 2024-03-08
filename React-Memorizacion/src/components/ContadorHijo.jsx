a02b1b0a2fb90738ac0e3c0d74c0f9885f716962
Manuel lopez <manuelmzt44@gmail.com>
2024-03-07 17:04:29 -0700

    React - useMemo

--- React-Memorizacion/src/components/ContadorHijo.jsx
+++ React-Memorizacion/src/components/ContadorHijo.jsx
@@ -1,7 +1,21 @@
-import { memo } from "react";
+import { memo, useMemo } from "react";
 
 const ContadorHijo = ({ contador, sumar, restar }) => {
   console.log("Hijo Contador se renderiza");
+
+  const superNumero = useMemo(()=>{
+      let numero = 0
+
+      for(let i=0;i<1000000000;i++) {
+        numero++
+      }
+
+      return numero
+
+  },[])
+
+
+
   return (
     <div style={{ border: "thin solid #000", padding: "1rem", margin: "1rem" }}>
       <h2>Hijo del Contador</h2>
@@ -10,6 +24,7 @@ const ContadorHijo = ({ contador, sumar, restar }) => {
         <button onClick={sumar}>+</button>
         <button onClick={restar}>-</button>
       </nav>
+      <h3>{superNumero}</h3>
     </div>
   );
 };
